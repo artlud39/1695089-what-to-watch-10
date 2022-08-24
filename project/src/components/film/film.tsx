427d772dd276a8e5ee9f58bcf0a041185926917f
artem.zholud <zholud.artem@gmail.com>
2022-08-24 13:47:07 +0300

    удалил лишнее

--- project/src/components/film/film.tsx
+++ project/src/components/film/film.tsx
@@ -1,5 +1,4 @@
-import { generatePath, Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import { Link } from 'react-router-dom';
 import VideoPlayer from '../video-player/video-player';
 import { FilmType } from '../../types/films';
 
